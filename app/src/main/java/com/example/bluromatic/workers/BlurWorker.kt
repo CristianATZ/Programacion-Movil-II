620c03661feb2dd9a1c00b78403e36b8249b0ada
AlanSalgado <s20120202@alumnos.itsur.edu.mx>
2024-02-08 19:17:03 -0600

    Practica terminada. La app ya difumina fotos

--- app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -2,9 +2,11 @@ package com.example.bluromatic.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import androidx.work.workDataOf
 import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.R
@@ -28,28 +30,39 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
         return withContext(Dispatchers.IO){
             return@withContext try {
                 require(!resoureUri.isNullOrBlank()){
-
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                        Log.e(TAG, errorMessage)
+                        errorMessage
                 }
+                val resolver = applicationContext.contentResolver
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resoureUri))
+                )
 
                 delay(2000)
 
-                val picture = BitmapFactory.decodeResource(
-                    applicationContext.resources,
-                    R.drawable.android_cupcake
-                )
+                //val picture = BitmapFactory.decodeResource(
+                //    applicationContext.resources,
+                //    R.drawable.android_cupcake
+                //)
 
-                val output = blurBitmap(picture, 2)
+                // val output = blurBitmap(picture, 2)
+                val output = blurBitmap(picture, blurLevel)
 
                 // escribe el mapa de bits a un archivo
                 val outputUri = writeBitmapToFile(applicationContext, output)
 
                 // muestra la notificacion
-                makeStatusNotification(
-                    "Output is $outputUri",
-                    applicationContext
-                )
+                //makeStatusNotification(
+                //    "Output is $outputUri",
+                //    applicationContext
+                //)
 
-                Result.success()
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+                //Result.success()
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
