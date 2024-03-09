7511127834d8349a11581503c6fba5a981e2ffc3
Azucena Camargo Ruiz <s20120176@alumnos.itsur.edu.mx>
2024-03-09 14:59:00 -0600

    Se agregó las entities faltantes

--- app/src/main/java/com/example/accesologin/model/Entities.kt
+++ app/src/main/java/com/example/accesologin/model/Entities.kt
@@ -37,4 +37,91 @@ data class Alumno_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
     @ColumnInfo(name = "fecha") val fecha: String = ""
 )
 
+@Entity(tableName = "carga")
+data class Carga_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo(name = "Semipresencial") val Semipresencial: String = "",
+    @ColumnInfo(name = "Observaciones") val Observaciones: String = "",
+    @ColumnInfo(name = "Docente") val Docente: String = "",
+    @ColumnInfo(name = "clvOficial") val clvOficial: String = "",
+    @ColumnInfo(name = "Sabado") val Sabado: String = "",
+    @ColumnInfo(name = "Viernes") val Viernes: String = "",
+    @ColumnInfo(name = "Jueves") val Jueves: String = "",
+    @ColumnInfo(name = "Miercoles") val Miercoles: String = "",
+    @ColumnInfo(name = "Martes") val Martes: String = "",
+    @ColumnInfo(name = "Lunes") val Lunes: String = "",
+    @ColumnInfo(name = "EstadoMateria") val EstadoMateria: String = "",
+    @ColumnInfo(name = "CreditosMateria") val CreditosMateria: String = "",
+    @ColumnInfo(name = "Materia") val Materia: String = "",
+    @ColumnInfo(name = "Grupo") val Grupo: String = "",
+    @ColumnInfo(name = "fecha") val fecha: String = ""
+)
 
+
+@Entity(tableName = "califUnidad")
+data class CalifUnidad_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo(name = "Observaciones") val Observaciones: String = "",
+    @ColumnInfo(name = "C13") val C13: String = "",
+    @ColumnInfo(name = "C12") val C12: String = "",
+    @ColumnInfo(name = "C11") val C11: String = "",
+    @ColumnInfo(name = "C10") val C10: String = "",
+    @ColumnInfo(name = "C9") val C9: String = "",
+    @ColumnInfo(name = "C8") val C8: String = "",
+    @ColumnInfo(name = "C7") val C7: String = "",
+    @ColumnInfo(name = "C6") val C6: String = "",
+    @ColumnInfo(name = "C5") val C5: String = "",
+    @ColumnInfo(name = "C4") val C4: String = "",
+    @ColumnInfo(name = "C3") val C3: String = "",
+    @ColumnInfo(name = "C2") val C2: String = "",
+    @ColumnInfo(name = "C1") val C1: String = "",
+    @ColumnInfo(name = "UnidadesActivas") val UnidadesActivas: String = "",
+    @ColumnInfo(name = "Materia") val Materia: String = "",
+    @ColumnInfo(name = "Grupo") val Grupo: String = "",
+    @ColumnInfo(name = "fecha") val fecha: String = ""
+)
+
+
+@Entity(tableName = "califFinal")
+data class CalifFinal_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo(name = "calif") val calif: String = "",
+    @ColumnInfo(name = "acred") val acred: String = "",
+    @ColumnInfo(name = "grupo") val grupo: String = "",
+    @ColumnInfo(name = "materia") val materia: String = "",
+    @ColumnInfo(name = "Observaciones") val Observaciones: String = "",
+    @ColumnInfo(name = "fecha") val fecha: String = ""
+)
+
+@Entity(tableName = "cardex")
+data class Cardex_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo(name = "ClvMat") val ClvMat: String = "",
+    @ColumnInfo(name = "ClvOfiMat") val ClvOfiMat: String = "",
+    @ColumnInfo(name = "Materia") val Materia: String = "",
+    @ColumnInfo(name = "Cdts") val Cdts: String = "",
+    @ColumnInfo(name = "Calif") val Calif: String = "",
+    @ColumnInfo(name = "Acred") val Acred: String = "",
+    @ColumnInfo(name = "S1") val S1: String = "",
+    @ColumnInfo(name = "P1") val P1: String = "",
+    @ColumnInfo(name = "A1") val A1: String = "",
+    @ColumnInfo(name = "S2") val S2: String = "",
+    @ColumnInfo(name = "P2") val P2: String = "",
+    @ColumnInfo(name = "A2") val A2: String = "",
+    @ColumnInfo(name = "S3") val S3: String = "",
+    @ColumnInfo(name = "P3") val P3: String = "",
+    @ColumnInfo(name = "A3") val A3: String = "",
+    @ColumnInfo(name = "fecha") val fecha: String = ""
+)
+
+@Entity(tableName = "cardexProm")
+data class CardexProm_Entity @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey(true) val id: Int = 0,
+    @ColumnInfo(name = "PromedioGral") val PromedioGral: String = "",
+    @ColumnInfo(name = "CdtsAcum") val CtdsAcum: String = "",
+    @ColumnInfo(name = "CdtsPlan") val CdtsPlan: String = "",
+    @ColumnInfo(name = "MatCursadas") val MatCursadas: String = "",
+    @ColumnInfo(name = "MatAprobadas") val MatAprobadas: String = "",
+    @ColumnInfo(name = "AvanceCtds") val AvanceCdts: String = "",
+    @ColumnInfo(name = "fecha") val fecha: String = ""
+)
